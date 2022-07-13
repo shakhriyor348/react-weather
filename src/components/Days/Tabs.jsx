88569ae359844785e64a235e1466a5f3499514ff
shakhriyor348 <62033235+shakhriyor348@users.noreply.github.com>
2022-07-13 23:49:25 +0300

    added tabs

--- src/components/Days/Tabs.jsx
+++ src/components/Days/Tabs.jsx
@@ -19,7 +19,7 @@ const Tabs = () => {
             <div className={classes['tabs__wrapper']}>
                 {
                     tabs.map((tab) => (
-                        <div className={classes['tabs__tab']} key={tab.value}>{tab.value}</div>
+                        <div className={classes['tabs__tab'] + ' ' + classes['active']} key={tab.value}>{tab.value}</div>
                     ))
                 }
             </div>
